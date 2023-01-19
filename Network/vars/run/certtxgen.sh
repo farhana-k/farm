2862e3dde065193e3688d1422ca88098e6b2ab8b
FarhanaKottal <farhana.kotal@gmail.com>
2023-01-19 23:33:58 +0530

    update

---
+++ Network/vars/run/certtxgen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $FABRIC_CFG_PATH
+# cryptogen generate --config crypto-config.yaml --output keyfiles
+configtxgen -profile OrdererGenesis -outputBlock genesis.block -channelID systemchannel
+
+configtxgen -printOrg buyer-agro-com > JoinRequest_buyer-agro-com.json
+configtxgen -printOrg farmer-agro-com > JoinRequest_farmer-agro-com.json
